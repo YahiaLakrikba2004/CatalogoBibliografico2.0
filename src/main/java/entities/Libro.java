80ebaa8d58ea9cd071e0c6bd5196558ec6810def
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-06-07 12:25:51 +0200

    Libro, periodicita e Rivista
    creazioni di 3 classi (LIBRO, PERIODICITA, RIVISTA)

--- src/main/java/entities/Libro.java
+++ src/main/java/entities/Libro.java
@@ -1,4 +1,43 @@
 package entities;
 
-public class Libro {
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Libro extends ElementoLetterario {
+    private String autore;
+    private String genere;
+
+
+    public Libro(int codiceIsbn, String titolo, int annoDiPubblicazione, int numeroDiPagine, String autore, String genere) {
+        super(codiceIsbn, titolo, annoDiPubblicazione, numeroDiPagine);
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    @Override
+    public String toString() {
+       return " {" +
+               "autore =" + autore +
+               ", titolo=" + getTitolo() +
+               ", genere" + getGenere() +
+               '}';
+    }
 }
---
+++ src/main/java/entities/Rivista.java
@@ -0,0 +1,36 @@
+package entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Entity
+public class Rivista extends ElementoLetterario {
+    @Enumerated(EnumType.STRING)
+    Periodicita periodicita;
+
+    public Rivista(int codiceIsbn, String titolo, int annoDiPubblicazione, int numeroDiPagine, Periodicita periodicita) {
+        super(codiceIsbn, titolo, annoDiPubblicazione, numeroDiPagine);
+        this.periodicita = periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    public void setPeriodicita(Periodicita periodicita) {
+        this.periodicita = periodicita;
+    }
+
+    @Override
+    public String toString() {
+        return "Rivista {" +
+                "titolo=" + getTitolo() +
+                " periodicita=" + periodicita +
+                '}';
+    }
+
+
+
+}
